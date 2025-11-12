02f7db2bbfe0ca9ff5832bccbfd6f8ffc14096db
Sheharyar45 <meghanisheheryar@gmail.com>
2025-11-12 07:49:25 +0000

    Added our tables

--- Main.java
+++ Main.java
@@ -7,70 +7,46 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
-	public static void main(String[] args) {
-		Connection conn = null;
-		Statement stmt = null;
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
 
-		try {
-			// 
-			// Register the PostgreSQL driver
-			//
-			//
-			Class.forName("org.postgresql.Driver");
+        try {
+            // Register the PostgreSQL driver
+            Class.forName("org.postgresql.Driver");
 
-			//
-			// Connect to the database
-			//
-			//
-			conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/mydb", "postgres", "postgres");
-			System.out.println("Opened database successfully");
+            // Connect to the database
+            conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/mydb", "postgres", "postgres");
+            System.out.println("Opened database successfully");
 
-			//
-			// Create a statement object
-			//
-			//
-			stmt = conn.createStatement();
+            // Create a statement object
+            stmt = conn.createStatement();
 
-			//
-			// Create SQL statement to insert a tuple
-			//
-			//
-			String sqlInsert = "INSERT INTO testtbl (name, value) " +
-				"VALUES ('world', 1024);";
-			stmt.executeUpdate(sqlInsert);
-			System.out.println("Tuple inserted successfully");
+            // Query the first 10 rows from HistoricData
+            String sqlCheck = "SELECT timestamp, Stock_symbol, close FROM HistoricData ORDER BY timestamp DESC LIMIT 10;";
+            ResultSet rsCheck = stmt.executeQuery(sqlCheck);
 
-			//
-			// Create SQL statement to query all tuples
-			//
-			//
-			String sqlSelect = "SELECT name, value FROM testtbl;";
-			ResultSet rs = stmt.executeQuery(sqlSelect);
-
-			//
-			// Print the queried tuples
-			//
-			//
-			System.out.println("Table testtbl contains the following tuples:\nname \tvalue");
-			while (rs.next()) {
-				String name = rs.getString("name");
-				int value = rs.getInt("value");
-				System.out.println(name + " \t" + value);
-			}
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			System.exit(1);
-		} finally {
-			try {
-				if (stmt != null) stmt.close();
-				if (conn != null) conn.close();
-			
-				System.out.println("Disconnected from the database");
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+            System.out.println("First 10 rows of HistoricData:");
+            System.out.println("Timestamp \tSymbol \tClose");
+            while (rsCheck.next()) {
+                String timestamp = rsCheck.getDate("timestamp").toString();
+                String symbol = rsCheck.getString("Stock_symbol");
+                float close = rsCheck.getFloat("close");
+                System.out.println(timestamp + "\t" + symbol + "\t" + close);
+            }
+            rsCheck.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(1);
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
+                System.out.println("Disconnected from the database");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
